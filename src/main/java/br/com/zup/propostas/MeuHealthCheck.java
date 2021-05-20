01e42eea5c573984653b4c253364452c172f818c
andreevilela <andrevibegoogle@hotmail.com>
2021-05-20 12:21:44 -0300

    020.melhorando_visibilidade_healthcheck

---
+++ src/main/java/br/com/zup/propostas/MeuHealthCheck.java
@@ -0,0 +1,20 @@
+package br.com.zup.propostas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class MeuHealthCheck implements HealthIndicator {
+	
+	@Override
+    public Health health() {
+        Map<String, Object> details = new HashMap<>();
+        details.put("versão", "1.2.3");
+        details.put("descrição", "Aqui pode vir alguma informação customizada caso solicitado");
+        details.put("endereço", "127.0.0.1");
+        return Health.up().withDetails(details).build();
+    }
+
+}
